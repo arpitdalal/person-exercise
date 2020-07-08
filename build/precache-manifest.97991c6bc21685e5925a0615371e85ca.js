6b124993b238100b1c9b2026338782c0854413d4
arpitdalal <arpitdalalm@gmail.com>
2020-07-08 18:28:02 -0300

    added build folder

---
+++ build/precache-manifest.97991c6bc21685e5925a0615371e85ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6251402504dfcb0395afbc82ad730d5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38cfb67016830bebbe1d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f0ef783fe834c8a9505",
+    "url": "/static/js/2.71abcb15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71abcb15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cfb67016830bebbe1d",
+    "url": "/static/js/main.c10ce160.chunk.js"
+  },
+  {
+    "revision": "d6e0e49e2415d5cc46b4",
+    "url": "/static/js/runtime-main.c338c4b7.js"
+  }
+]);
